8d781817c3f2e6e0e10a54cb605a1c24f2db6701
Bob Mwenda <bmwenda@mgic.umaryland.edu>
2021-09-16 14:53:40 +0300

    updated the table with employees data

---
+++ database/migrations/2021_09_16_115057_create_employees_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->primary();
+            $table->string('name', 191);
+            $table->string('date_of_birth', 191);
+            $table->string('location', 191);
+            $table->string('email', 191);
+            $table->string('username', 191);
+            $table->string('timezone', 191);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrentOnUpdate()->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
